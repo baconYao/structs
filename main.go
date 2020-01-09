a58f401a1d5dcdce3164faf0b61a68753bff1837
baconYao <bacon735392@gmail.com>
2020-01-09 21:31:31 +0800

    added the tricky snippet to help us understand the pointer and the mean of "*everything* is passed by value in Go"

--- main.go
+++ main.go
@@ -41,6 +41,23 @@ func main() {
 	// 在這裡演示了不用像上面的alex一樣宣告成指向記憶體變數的參數，go也會自動幫你處理掉(只要你的receiver的type是指標)
 	peter.updateName("Gary")
 	peter.print()
+
+
+	// 在更新slice時，不需要指標的形式
+	mySlice := []string{"Hi", "There", "How", "Are", "You"}
+	updateSlice(mySlice)
+	fmt.Println(mySlice)
+
+
+	// 值得觀察的一個片段，看看A,B,C,D有哪些是一樣的值
+	name := "bill"
+ 
+	namePointer := &name
+	
+	fmt.Println("A: ", namePointer)
+	fmt.Println("B: ", &namePointer)
+	printPointer(namePointer)
+
 }
 
 // *person 代表的是這個receiver type要的是指標
@@ -52,3 +69,12 @@ func (pointerToPerson *person) updateName(newFirstName string) {
 func (p person) print() {
 	fmt.Printf("%+v\n", p)
 }
+
+func updateSlice(s []string) {
+	s[0] = "Bye"
+}
+
+func printPointer(namePointer *string) {
+	fmt.Println("C: ", namePointer)
+	fmt.Println("D: ", &namePointer)
+   }
